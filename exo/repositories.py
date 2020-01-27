34e52d7bf302d25466f9e201dd718565e39196a7
Zech Zimmerman <zechzimm@gmail.com>
2020-01-27 18:57:09 -0500

    Create repository implementation

--- exo/repositories.py
+++ exo/repositories.py
@@ -1,9 +1,130 @@
 from __future__ import annotations
-from typing import TypeVar
+from abc import ABC, abstractmethod
+from typing import Any, Type, TypeVar
 
 
-ExoRepository = TypeVar("ExoRepository", bound="Repository")
+ExoRepository = TypeVar("ExoRepository", bound="AbstractRepository")
 
 
-class Repository:
-    ...
+class AbstractRepository(ABC):
+    @property
+    @abstractmethod
+    def parent(self) -> ExoRepository:
+        return NullRepository()
+
+    @abstractmethod
+    def create_child(self) -> ExoRepository:
+        return NullRepository()
+
+    @abstractmethod
+    def get(self, cls: Type) -> Any:
+        return
+
+    @abstractmethod
+    def has(self, cls: Type, *, propagate: bool = False) -> bool:
+        return False
+
+    def is_null(self) -> bool:
+        return self is NullRepository()
+
+
+class NullRepository(AbstractRepository):
+    """ Null singleton repository. """
+
+    null_instance = None  # Holds the singleton instance
+
+    def __new__(cls, *args, **kwargs):
+        """ Caches a single instance that is always returned. """
+        if not NullRepository.null_instance:
+            NullRepository.null_instance = super().__new__(cls, *args, **kwargs)
+        return NullRepository.null_instance
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @property
+    def parent(self) -> ExoRepository:
+        """ Returns a null repository. It is important that when traversing
+        upwards through the parent hierarchy that there are checks for a null
+        repository to prevent infinite loops/recursion. """
+        return self
+
+    def create_child(self) -> ExoRepository:
+        return self
+
+    def get(self, cls: Type) -> Any:
+        """ Doesn't load anything and returns None instead. """
+        return None
+
+    def has(self, cls: Type, *, propagate: bool = False) -> bool:
+        """ Doesn't have any instances. """
+        return False
+
+
+class Repository(AbstractRepository):
+    """ Simple repository implementation for storing instances of classes for
+    use by multiple objects. """
+
+    def __init__(self, parent: ExoRepository = NullRepository()):
+        self._parent = parent
+        self._repository = {}
+
+    @property
+    def parent(self) -> ExoRepository:
+        return self._parent
+
+    def create_child(self) -> ExoRepository:
+        return self.__class__(self)
+
+    def get(self, cls: Type) -> Any:
+        instance = self._get(cls)
+        if not instance:
+            instance = self._create_instance(cls)
+
+        return instance
+
+    def has(self, cls: Type, *, propagate: bool = True) -> bool:
+        """ Checks if the repository or any of it's parents have an instance of
+        the requested class. Propagation to parents can be disabled with the
+        "propagate" boolean keyword argument. """
+        if not propagate:
+            return cls in self._repository
+
+        return cls in self._repository or self.parent.has(cls)
+
+    def _build_instance(self, cls: Type) -> Any:
+        """ Builds an instance of a class. This will look for a
+        __repository_build__ class method which it can call to get an instance
+        built correctly for the repository. If that class method isn't found it
+        will fallback to the standard Python constructor. """
+        if hasattr(cls, "__repository_build__"):
+            return cls.__repository_build__()
+
+        return cls()
+
+    def _create_instance(self, cls: Type) -> Any:
+        """ Creates and stores an instance of a class placing it in the
+        repository if appropriate. """
+        instance = self._build_instance(cls)
+        self._store_instance(instance, cls)
+        return instance
+
+    def _get(self, cls: Type) -> Any:
+        """ Gets an instance of a class from the repository or it's parents if
+        it has been created, returns None if it's not found. """
+        if not self.has(cls):
+            return None
+
+        if cls in self._repository:
+            return self._repository[cls]
+
+        return self.parent.get(cls)
+
+    def _store_instance(self, instance: Any, cls: Type):
+        """ Stores an instance in the repository. It checks of an
+        __repository_ignore__ class attribute that is set to True, if found it
+        will ignore the instance and not store it. """
+        if hasattr(cls, "__repository_ignore__") and cls.__repository_ignore__:
+            return
+
+        self._repository[cls] = instance
---
+++ tests/test_repositories.py
@@ -0,0 +1,98 @@
+from unittest import TestCase
+from exo.repositories import NullRepository, Repository
+
+
+class TestNullRepository(TestCase):
+    def test_is_null(self):
+        repo = NullRepository()
+
+        self.assertTrue(repo.is_null(), "Null Repository is not null")
+
+    def test_not_null(self):
+        repo = Repository()
+
+        self.assertFalse(repo.is_null(), "Repository is null")
+
+
+class TestRepository(TestCase):
+    def test_parent(self):
+        parent_repo = Repository()
+        repo = parent_repo.create_child()
+
+        self.assertIs(parent_repo, repo.parent, "The parent is not set correctly")
+
+    def test_parent_null(self):
+        repo = Repository()
+
+        self.assertIs(repo.parent, NullRepository(), "The parent must be null")
+
+    def test_get(self):
+        class Test:
+            pass
+
+        repo = Repository()
+        inst = repo.get(Test)
+
+        self.assertIsInstance(
+            inst, Test, "Repository didn't not return an instance of the correct type"
+        )
+
+    def test_get_single_instance(self):
+        class Test:
+            pass
+
+        repo = Repository()
+        inst = repo.get(Test)
+        inst2 = repo.get(Test)
+
+        self.assertIs(inst, inst2, "Repository instantiated multiple instances")
+
+    def test_get_inheritance(self):
+        class Test:
+            pass
+
+        repo = Repository()
+        child_repo = repo.create_child()
+
+        inst = repo.get(Test)
+        inst2 = child_repo.get(Test)
+
+        self.assertIs(inst, inst2, "Repository inheritance failed")
+
+    def test_get_reverse_inheritance(self):
+        class Test:
+            pass
+
+        repo = Repository()
+        child_repo = repo.create_child()
+
+        inst = child_repo.get(Test)
+        inst2 = repo.get(Test)
+
+        self.assertIsNot(inst, inst2, "Repository inheritance failed")
+
+    def test_repository_ignore(self):
+        class Test:
+            __repository_ignore__ = True
+
+        repo = Repository()
+
+        inst1 = repo.get(Test)
+        inst2 = repo.get(Test)
+
+        self.assertIsNot(inst1, inst2, "Repository instantiated multiple times")
+
+    def test_repository_build(self):
+        sentinel = object()
+
+        class Test:
+            @classmethod
+            def __repository_build__(cls):
+                return sentinel
+
+        repo = Repository()
+        number = repo.get(Test)
+
+        self.assertIs(
+            number, sentinel, "Repository failed to correctly call build method"
+        )
