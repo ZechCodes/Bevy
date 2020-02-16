4ab3b53d42f0ba82bfa0d7c5d2cabbd0f62756ea
Zech Zimmerman <zechzimm@gmail.com>
2020-02-16 13:39:57 -0500

    Implement repository get/has/set methods

--- exo/repository.py
+++ exo/repository.py
@@ -3,15 +3,68 @@ from typing import Any, List, Dict, Optional, Tuple, Type, TypeVar, Union
 
 
 GenericRepository = TypeVar("GenericRepository", bound="Repository")
+GenericInstance = TypeVar("GenericInstance")
+GenericType = Type[GenericInstance]
+_NOVAL = object()
 
 
 class Repository:
+    def __init__(self):
+        self._instance_repo: Dict[GenericType, GenericInstance] = {}
+
+    def get(
+        self, obj: GenericType, *, default: Any = _NOVAL
+    ) -> Optional[GenericInstance]:
+        """ Get's an instance matching the requested type from the repository.
+        If default is not set and an match is not found this will create an
+        instance using the requested type. """
+        if not self.has(obj):
+            if default is not _NOVAL:
+                return default
+            return self.set(obj, obj)
+        return self._find(obj)
+
+    def has(self, obj: GenericType) -> bool:
+        """ Checks if an instance matching the requested type exists in the
+        repository. If a type is not provided this will raise an exception. """
+        if not isinstance(obj, type):
+            raise ExoRepositoryMustBeType(f"Repository expected a type received {obj}")
+
+        return self._find(obj) is not _NOVAL
+
+    def set(
+        self, look_up_type: GenericType, instance: Union[GenericType, GenericInstance]
+    ) -> GenericInstance:
+        """ Sets the instance that should be returned when a given type is
+        requested. This will raise exceptions if the look up type isn't a type
+        and if the instance type is not an instance of the look up type. """
+        if not isinstance(look_up_type, type):
+            raise ExoRepositoryMustBeType(
+                f"Repository expected a type received {look_up_type}"
+            )
+
+        value = instance() if isinstance(instance, type) else instance
+
+        if not isinstance(value, look_up_type):
+            raise ExoRepositoryMustBeMatchingTypes(
+                f"Cannot set a value for mismatched types, received {look_up_type} and {instance}"
+            )
+
+        self._instance_repo[look_up_type] = value
+        return value
+
+    def _find(self, obj: GenericType) -> Union[GenericInstance, _NOVAL]:
+        for obj_type in self._instance_repo:
+            if issubclass(obj, obj_type):
+                return self._instance_repo[obj_type]
+        return _NOVAL
+
     @classmethod
     def create(
         cls,
         repo: Optional[Union[GenericRepository, Type[GenericRepository]]] = None,
         *args,
-        **kwargs
+        **kwargs,
     ) -> GenericRepository:
         """ Return a repository object. If the repo provided is already
         instantiated it will be returned without change. If it is a subclass of
@@ -26,3 +79,11 @@ class Repository:
             return repo(*args, **kwargs)
 
         return cls(*args, **kwargs)
+
+
+class ExoRepositoryMustBeType(Exception):
+    ...
+
+
+class ExoRepositoryMustBeMatchingTypes(Exception):
+    ...
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
-from exo.repository import Repository
+from exo.repository import (
+    Repository,
+    ExoRepositoryMustBeMatchingTypes,
+    ExoRepositoryMustBeType,
+)
 
 
 class TestRepository(TestCase):
@@ -50,4 +54,87 @@ class TestRepository(TestCase):
             Repository.create(Repo),
             Repo,
             "Failed to create an instance of the repository subclass",
+        )
+
+    # TEST GET/HAS/SET
+    def test_set_instance(self):
+        repo = Repository()
+        sentinel = object()
+
+        self.assertEqual(
+            repo.set(object, sentinel),
+            sentinel,
+            "Failed to set the value in the repository",
+        )
+
+    def test_set_type(self):
+        repo = Repository()
+
+        self.assertEqual(
+            repo.set(str, str),
+            "",
+            "Failed to instantiate and set the value in the repository",
+        )
+
+    def test_set_unmatched(self):
+        repo = Repository()
+
+        with self.assertRaises(
+            ExoRepositoryMustBeMatchingTypes, msg="Failed to detect unmatched types"
+        ):
+            repo.set(str, 1)
+
+    def test_set_non_type(self):
+        repo = Repository()
+
+        with self.assertRaises(
+            ExoRepositoryMustBeType, msg="Failed to detect non-type"
+        ):
+            repo.set("", "")
+
+    def test_not_has(self):
+        repo = Repository()
+
+        self.assertFalse(
+            repo.has(str), "Has check found something when it shouldn't have"
+        )
+
+    def test_has_non_type(self):
+        repo = Repository()
+
+        with self.assertRaises(
+            ExoRepositoryMustBeType, msg="Failed to detect non-type"
+        ):
+            repo.has("")
+
+    def test_has(self):
+        repo = Repository()
+        repo.set(str, "")
+
+        self.assertTrue(repo.has(str), "Failed to find existing instance")
+
+    def test_get_non_type(self):
+        repo = Repository()
+
+        with self.assertRaises(
+            ExoRepositoryMustBeType, msg="Failed to detect non-type"
+        ):
+            repo.get("")
+
+    def test_get_existing(self):
+        repo = Repository()
+        repo.set(str, "")
+
+        self.assertEqual(repo.get(str), "", "Failed to find existing instance")
+
+    def test_get_instantiate(self):
+        repo = Repository()
+
+        self.assertEqual(repo.get(str), "", "Failed to create instance")
+
+    def test_get_default(self):
+        repo = Repository()
+
+        self.assertEqual(
+            repo.get(str, default="NOVAL"), "NOVAL", "Failed to create instance"
         )
