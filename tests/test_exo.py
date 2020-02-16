ca9436b09f0e45bea8f830a40793df26d7bcf0c4
Zech Zimmerman <zechzimm@gmail.com>
2020-02-16 14:23:43 -0500

    Add test to ensure that dependencies are shared

--- tests/test_exo.py
+++ tests/test_exo.py
@@ -13,3 +13,30 @@ class TestExeDependencies(TestCase):
         self.assertEqual(
             App.__dependencies__, {"dep": Dep}, "Dependencies failed to set correctly"
         )
+
+    def test_dependency_injection(self):
+        class Dep(Exo):
+            ...
+
+        class App(Exo):
+            dep: Dep
+
+        app = App()
+        self.assertTrue(
+            hasattr(app, "dep"), "Failed to inject the dependency with the correct name"
+        )
+        self.assertIsInstance(app.dep, Dep, "Dependency is of the wrong type")
+
+    def test_dependency_injection_sharing(self):
+        class Dep(Exo):
+            ...
+
+        class App(Exo):
+            dep: Dep
+
+        class App2(Exo):
+            dep: Dep
+
+        app = App()
+        app2 = App2(__repository__=app.__repository__)
+        self.assertIs(app.dep, app2.dep, "Dependency was not shared")
