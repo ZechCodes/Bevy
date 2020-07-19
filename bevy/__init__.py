412dab091138d7628d2480b488a5d751bee82f01
Zech Zimmerman <hi@zech.codes>
2020-07-19 15:06:25 -0400

    Update bevy package imports

--- bevy/__init__.py
+++ bevy/__init__.py
@@ -1,6 +1,7 @@
 from bevy.bevy import Bevy
+from bevy.context import Context
 from bevy.factory import Factory
 
 
-__all__ = ["Bevy"]
+__all__ = ["Bevy", "Context", "Factory"]
 __version__ = "0.2.3"
