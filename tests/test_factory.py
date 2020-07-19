80cce0a99ba165952bbfce26cb4c087c7df73ff5
Zech Zimmerman <hi@zech.codes>
2020-07-19 15:02:14 -0400

    Update factory tests

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,52 +1,61 @@
 from pytest import fixture
-from bevy import Bevy, Factory
+from bevy import Bevy, Context, Factory
 
 
-class TestBuilder:
-    @fixture
-    def dependency(self, sub_dependency):
-        class Dep(Bevy):
-            sub: sub_dependency
+@fixture
+def dependency(sub_dependency):
+    class Dep(Bevy):
+        sub: sub_dependency
 
-        return Dep
+    return Dep
 
-    @fixture
-    def sub_dependency(self):
-        class SubDep:
-            ...
 
-        return SubDep
+@fixture
+def sub_dependency():
+    class SubDep:
+        ...
 
-    @fixture
-    def app(self, dependency):
-        class App(Bevy):
-            dep_factory: Factory[dependency]
+    return SubDep
 
-            def get(self) -> dependency:
-                return self.dep_factory()
 
-        return App
+@fixture
+def app(dependency):
+    class App(Bevy):
+        dep_factory: Factory[dependency]
 
-    def test_factory(self, app, dependency):
-        a = app()
+        def get(self) -> dependency:
+            return self.dep_factory()
 
-        assert isinstance(a.get(), dependency)
+    return App
 
-    def test_distinct_instances(self, app, dependency):
-        a = app()
 
-        assert a.get() is not a.get()
+# ###   TESTS   ### #
 
-    def test_deps_inherited(self, app, dependency, sub_dependency):
-        s = sub_dependency()
-        a = app.context(s).build()
 
-        x = a.get().sub
-        assert x is s
+def test_factory(app, dependency):
+    a = app()
 
-    def test_same_deps(self, app, dependency, sub_dependency):
-        a = app()
+    assert isinstance(a.get(), dependency)
 
-        x = a.get().sub
-        y = a.get().sub
-        assert x is y
+
+def test_distinct_instances(app, dependency):
+    a = app()
+
+    assert a.get() is not a.get()
+
+
+def test_deps_inherited(app, dependency, sub_dependency):
+    s = sub_dependency()
+    c = Context().load(s)
+    a = c.get(app)
+
+    x = a.get().sub
+    assert x is s
+
+
+def test_same_deps(app, dependency, sub_dependency):
+    a = app()
+
+    x = a.get().sub
+    y = a.get().sub
+    assert x is y
