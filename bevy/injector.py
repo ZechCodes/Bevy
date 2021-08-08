7c2a9b8ebcb95d34ec37f6634ddf404d72efa576
Zech Zimmerman <hi@zech.codes>
2021-08-08 16:35:06 -0400

    Updated docs

--- bevy/injector.py
+++ bevy/injector.py
@@ -4,6 +4,12 @@ import bevy
 
 
 class Injector(Protocol):
+    """Protocol that allows a class to modify how and what it injects into another class.
+
+    When a class is being instantiated and having it's dependencies injected, Bevy will check if the dependency
+    implements the injector protocol. If it does the __bevy_inject__ method will be called and should handle injecting
+    into the class. This should include constructing whatever dependency is being injected."""
+
     @classmethod
     def __bevy_inject__(
         cls,
