bc5a25e2f8de22991606ef01e5632695c9b42627
Zech Zimmerman <hi@zech.codes>
2021-08-29 15:04:53 -0400

    Add tests for extension load policies

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from bevy.app.app import App
 from bevy.app.bootstrap import Bootstrap
+from bevy.app.settings import ExtensionLoadPolicy, ExtensionSettings
 import asyncio
 import pathlib
 import pytest
@@ -13,3 +14,33 @@ def working_directory():
 def test_app_creation(working_directory):
     app = Bootstrap(working_directory=working_directory).build()
     assert isinstance(app, App)
+
+
+def test_extension_load_policy_enabled_only():
+    enabled = ExtensionSettings(
+        "testing", {"enabled": True}, ExtensionLoadPolicy.ENABLED_ONLY
+    )
+    disabled = ExtensionSettings(
+        "testing", {"enabled": False}, ExtensionLoadPolicy.ENABLED_ONLY
+    )
+    not_set = ExtensionSettings(
+        "testing", {"enabled": None}, ExtensionLoadPolicy.ENABLED_ONLY
+    )
+    assert enabled.enabled is True
+    assert disabled.enabled is False
+    assert not_set.enabled is False
+
+
+def test_extension_load_policy_auto_load():
+    enabled = ExtensionSettings(
+        "testing", {"enabled": True}, ExtensionLoadPolicy.AUTO_ENABLE
+    )
+    disabled = ExtensionSettings(
+        "testing", {"enabled": False}, ExtensionLoadPolicy.AUTO_ENABLE
+    )
+    not_set = ExtensionSettings(
+        "testing", {"enabled": None}, ExtensionLoadPolicy.AUTO_ENABLE
+    )
+    assert enabled.enabled is True
+    assert disabled.enabled is False
+    assert not_set.enabled is True
