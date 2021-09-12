e51132bb38a789a39d81238ccb91daacb4051064
Zech Zimmerman <hi@zech.codes>
2021-09-11 23:00:47 -0400

    Add tests

--- bevy/__init__.py
+++ bevy/__init__.py
@@ -1,54 +1,5 @@
-"""Bevy is an object oriented *dependency injection* framework. Similar to Pydantic, it relies on Python 3's class
-annotations, using them to determine what dependencies a class has.
-
-**Example**
-```py
-@bevy.injectable
-class Example:
-    dependency: Dependency
-```
-Each dependency when instantiated is added to a context repository for reuse. This allows many classes to share the same
-instance of each dependency. This is handy for sharing things like database connections, config files, or authenticated
-API sessions.
-
-## Bevy Contexts
-
-To instantiate classes and have Bevy inject their dependencies it is necessary to use a
-[`bevy.Context`](#Context). The context takes a [`bevy.Injectable`](#Injectable) and any args necessary to
-instantiate it. Calling [`Context.build`](#Context.build) on the context will then create an instance of the
-`Injectable` with all dependencies injected.
-
-**Example**
-```py
-context = bevy.Context(Example)
-example = context.build()
-```
-### Configuring Dependencies
-
-When the `Context` encounters a dependency that is not in the context repository it will attempt to create the
-dependency. The approach is very naive, it will just call the dependency with no arguments. If it succeeds it will be
-added to the repository for later use and injected into the class.
-
-This behavior can be changed by passing an instantiated dependency to [`Context.add`](#Context.add).
-**Example**
-```py
-context.add(Dependency("foobar"))
-```
-When adding an `Injectable` it is necessary to use [`Context.branch`](#Context.branch) as it will inherit all
-dependencies that are added to the context. Any dependencies added to the branch will not be back propagated to the
-context, allowing for dependency isolation. Because branches inherit but do not propagate, their dependency
-resolution is deferred until `Context.build` is called, when it is assumed all dependencies with customized
-instantiations have been added.
-
-Because `Injectables` require a special lifecycle `Context.branch` will accept any instantiation args that should be
-passed to the class when it is constructed.
-**Example**
-```py
-branch = context.branch(Dependency)
-```
-"""
 from bevy.context import Context
-from bevy.injectable import injectable, is_injectable
+from bevy.inject import dependencies, Inject
 
 
-__all__ = ("Context", "injectable", "is_injectable")
+__all__ = ("Context", "dependencies", "Inject")
--- tests/test_bevy.py
+++ tests/test_bevy.py
@@ -1,193 +1,161 @@
-from bevy import Context, injectable, is_injectable
+from bevy import Context, dependencies, Inject, Injectable
 from bevy.factory import Factory
-from bevy.label import Label
 
 
-class Dependency:
-    def __init__(self, name):
-        self.name = name
-
-
-class DependencyB:
-    def __init__(self, name):
-        self.name = f"__{name}__"
-
-
-@injectable
-class BranchDep:
-    dep: Dependency
-
-
-@injectable
-class App:
-    dep: Dependency
-
-
-def test_simple_construction():
-    @injectable
-    class Testing:
-        def __init__(self):
-            ...
-
-    Testing()
-
-
-def test_for_double_init():
-    @injectable
-    class Testing:
-        msg = "only once"
-
-        def __init__(self):
-            assert Testing.msg == "only once"
-            Testing.msg = "called twice"
-
-    Testing()
-
-
-def test_construct():
-    context = Context(App)
-    context.add(Dependency("foobar"))
-    app = context.build()
-
-    assert app.dep.name == "foobar"
-
-
-def test_branching_inheritance():
-    @injectable
-    class App:
-        dep: BranchDep
-
-    context = Context(App)
-    context.add(Dependency("foobar"))
-    context.branch(BranchDep)
-    app = context.build()
-
-    assert app.dep.dep.name == "foobar"
-
-
-def test_branching_propagation():
-    context = Context(App)
-    context.add(Dependency("foobar"))
-    branch = context.branch(BranchDep)
-    branch.add(Dependency("hello world"))
-    app = context.build()
-
-    assert context.get(BranchDep).dep.name == "hello world"
-    assert app.dep.name == "foobar"
-
-
-def test_add_as():
-    context = Context(App)
-    context.add_as(DependencyB("foobar"), Dependency)
-    app = context.build()
-
-    assert app.dep.name == "__foobar__"
-
-
-def test_factories():
-    class Dep:
-        def __init__(self, name):
-            self.name = f"__{name}__"
-
-    @injectable
-    class App:
-        factory: Factory[Dep]
-
-    app = Context(App).build()
-    assert app.factory("foobar").name == "__foobar__"
-
-
-def test_injectable():
-    assert is_injectable(App)
-
-
-def test_no_context():
+def test_inject_dependencies():
     class Dep:
         ...
 
-    @injectable
-    class App:
-        dep: Dep
+    @dependencies
+    class Testing:
+        test: Inject[Dep]
 
-    assert App().dep
+    context = Context()
+    inst = context.build(Testing)
+
+    assert isinstance(inst.test, Dep)
 
 
-def test_label():
+def test_shared_dependencies():
     class Dep:
-        def __init__(self, value="default"):
-            self.value = value
+        ...
 
-    @injectable
-    class App:
-        dep: Label[Dep:"dep"]
+    @dependencies
+    class TestingA:
+        test: Inject[Dep]
 
-    app = Context(App).build()
-    assert isinstance(app.dep, Dep)
+    @dependencies
+    class TestingB:
+        test: Inject[Dep]
+
+    context = Context()
+    instA = context.build(TestingA)
+    instB = context.build(TestingB)
+
+    assert instA.test is instB.test
 
 
-def test_label_same_instance():
+def test_branching_same_dependencies():
     class Dep:
-        def __init__(self, value="default"):
-            self.value = value
+        ...
 
-    @injectable
-    class AppA:
-        dep: Label[Dep:"dep"]
+    @dependencies
+    class TestingA:
+        test: Inject[Dep]
 
-    @injectable
-    class AppB:
-        dep: Label[Dep:"dep"]
+    @dependencies
+    class TestingB:
+        test: Inject[Dep]
 
-    builder = Context(AppA)
-    app_b = builder.get(AppB)
-    app_a = builder.build()
-    assert app_a.dep is app_b.dep
+    context = Context()
+    branch = context.branch()
+    instA = context.build(TestingA)
+    instB = branch.build(TestingB)
+
+    assert instA.test is instB.test
 
 
-def test_label_different_instances():
+def test_branching_different_dependencies():
     class Dep:
-        def __init__(self, value="default"):
-            self.value = value
+        ...
 
-    @injectable
-    class AppA:
-        dep: Label[Dep:"dep_a"]
+    @dependencies
+    class TestingA:
+        test: Inject[Dep]
 
-    @injectable
-    class AppB:
-        dep: Label[Dep:"dep_b"]
+    @dependencies
+    class TestingB:
+        test: Inject[Dep]
 
-    builder = Context(AppA)
-    app_b = builder.get(AppB)
-    app_a = builder.build()
-    assert app_a.dep is not app_b.dep
+    context = Context()
+    branch = context.branch()
+    instB = branch.build(TestingB)
+    instA = context.build(TestingA)
+
+    assert instA.test is not instB.test
 
 
-def test_label_injection():
+def test_branching_different_dependencies():
     class Dep:
-        def __init__(self, value="default"):
-            self.value = value
+        ...
 
-    @injectable
-    class App:
-        dep: Label[Dep:"dep"]
+    @dependencies
+    class TestingA:
+        test: Inject[Dep]
 
-    builder = Context(App)
-    builder.add(Label(Dep("dep"), "dep"))
-    app = builder.build()
-    assert app.dep.value == "dep"
+    @dependencies
+    class TestingB:
+        test: Inject[Dep]
+
+    context = Context()
+    branch = context.branch()
+    instB = branch.build(TestingB)
+    instA = context.build(TestingA)
+
+    assert instA.test is not instB.test
 
 
-def test_ignoring_assigned_annotations():
+def test_dependencies_matching_labels():
     class Dep:
-        def __init__(self, value="default"):
-            self.value = value
+        ...
 
-    @injectable
-    class App:
-        dep: Dep = Dep()
+    @dependencies
+    class TestingA:
+        test: Inject[Dep, "testing"]
 
-    builder = Context(App)
-    builder.add(Dep("dep2"))
-    app = builder.build()
-    assert app.dep.value == "default"
+    @dependencies
+    class TestingB:
+        test: Inject[Dep, "testing"]
+
+    context = Context()
+    instA = context.build(TestingA)
+    instB = context.build(TestingB)
+
+    assert instA.test is instB.test
+
+
+def test_dependencies_nonmatching_labels():
+    class Dep:
+        ...
+
+    @dependencies
+    class TestingA:
+        test: Inject[Dep, "testingA"]
+
+    @dependencies
+    class TestingB:
+        test: Inject[Dep, "testingB"]
+
+    context = Context()
+    instA = context.build(TestingA)
+    instB = context.build(TestingB)
+
+    assert instA.test is instB.test
+
+
+def test_factory():
+    class Dep:
+        ...
+
+    @dependencies
+    class Testing:
+        test: Factory[Dep]
+
+    context = Context()
+    inst = context.build(Testing)
+
+    assert isinstance(inst.test(), Dep)
+
+
+def test_factory_unique():
+    class Dep:
+        ...
+
+    @dependencies
+    class Testing:
+        test: Factory[Dep]
+
+    context = Context()
+    inst = context.build(Testing)
+
+    assert inst.test() is not inst.test()
