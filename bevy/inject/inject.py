10a3e1dd045f9da44c236e2b009b2aa9c8592830
Zech Zimmerman <hi@zech.codes>
2022-06-08 22:14:31 -0400

    Set the detection stategy using the inject class kwarg

--- bevy/inject/inject.py
+++ bevy/inject/inject.py
@@ -7,7 +7,7 @@ from bevy.inject.inject_strategies import (
     InjectionStrategy,
     InjectAllStrategy,
     InjectAllowStrategy,
-    InjectDisallowStrategy
+    InjectDisallowStrategy,
 )
 
 
@@ -22,28 +22,26 @@ class Inject:
 
 class Bevy:
     __bevy_context__ = None
-    _detection_strategy = Detect.ONLY()
     bevy = ContextInjector()
 
-    def __init_subclass__(cls, **kwargs):
-        dependencies = cls._detection_strategy.get_declared_dependencies(cls)
-        cls._detection_strategy.create_injectors(cls, dependencies)
+    def __init_subclass__(cls, *, inject: InjectionStrategy | None = None, **kwargs):
+        strategy = inject or InjectAllowStrategy()
+        dependencies = strategy.get_declared_dependencies(cls)
+        strategy.create_injectors(cls, dependencies)
 
     def __class_getitem__(cls, strategy: InjectionStrategy) -> Type[Bevy]:
         return cast(
-            Type[Bevy],
-            type(
-                cls.__name__,
-                (cls,),
-                {
-                    "_detection_strategy": strategy
-                }
-            )
+            Type[Bevy], type(cls.__name__, (cls,), {"_detection_strategy": strategy})
         )
 
 
 class InjectionDescriptor(Generic[T]):
-    def __init__(self, on_cls: Type[Bevy], attr_name: str, annotation_getter: AnnotationGetter[Type[T], T]):
+    def __init__(
+        self,
+        on_cls: Type[Bevy],
+        attr_name: str,
+        annotation_getter: AnnotationGetter[Type[T], T],
+    ):
         self.on_cls = on_cls
         self.attr_name = attr_name
         self.annotation_getter = annotation_getter
@@ -51,6 +49,7 @@ class InjectionDescriptor(Generic[T]):
     @overload
     def __get__(self, instance: Bevy, owner) -> T:
         ...
+
     @overload
     def __get__(self, instance: None, owner) -> InjectionDescriptor:
         ...
@@ -60,4 +59,6 @@ class InjectionDescriptor(Generic[T]):
             return self
 
         type_hint = self.annotation_getter.get()
-        return instance.bevy.get(type_hint) or instance.bevy.create(type_hint, add_to_context=True)
+        return instance.bevy.get(type_hint) or instance.bevy.create(
+            type_hint, add_to_context=True
+        )
