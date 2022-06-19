0e2ce39b3354ce063bb05f57f2dce422548c674f
ZechCodes <hi@zech.codes>
2022-06-19 15:34:23 -0400

    Import the bevy_method decorator into the package dunder init

--- bevy/__init__.py
+++ bevy/__init__.py
@@ -1,3 +1,4 @@
+import bevy.providers as providers
 from bevy.context import Context
 from bevy.inject import Bevy, Inject
-import bevy.providers as providers
+from bevy.providers.function_provider import bevy_method
