8c83a57227689168509e3d59ffaa1aa60993c798
ZechCodes <hi@zech.codes>
2023-04-05 18:02:08 -0400

    Add tests for result monads

---
+++ tests/test_result_monad.py
@@ -0,0 +1,35 @@
+from bevy.results import Result, Success, Failure, ResultBuilder
+
+
+def test_not_set():
+    assert not Result()
+
+
+def test_result_set():
+    assert Success("Testing")
+
+
+def test_exception_set():
+    assert not Failure(Exception("Testing"))
+
+
+def test_result_builder_success():
+    with ResultBuilder() as (builder, set_result):
+        set_result("Testing")
+
+    match builder.result:
+        case Success(result):
+            assert result == "Testing"
+        case _:
+            assert False
+
+
+def test_result_builder_failure():
+    with ResultBuilder() as (builder, set_result):
+        raise Exception("Testing")
+
+    match builder.result:
+        case Failure(Exception() as exception):
+            assert exception.args == ("Testing",)
+        case _:
+            assert False
