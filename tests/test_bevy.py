902624fad4b0eb0c60997d51a5996f790ef84bf0
ZechCodes <hi@zech.codes>
2023-04-05 17:49:12 -0400

    Add basic functionality tests for getting from a repo

--- tests/test_bevy.py
+++ tests/test_bevy.py
@@ -3,3 +3,24 @@ from bevy import Repository, get_repository
 
 def test_repository_exists():
     assert isinstance(get_repository(), Repository)
+
+
+def test_repository_get():
+    class TestType:
+        ...
+
+    repo = get_repository()
+    instance = repo.get(TestType)
+
+    assert isinstance(instance, TestType)
+
+
+def test_repository_caching():
+    class TestType:
+        ...
+
+    repo = get_repository()
+    instance_a = repo.get(TestType)
+    instance_b = repo.get(TestType)
+
+    assert instance_a is instance_b
