df922dbd6d414a8ad6a920300456c11696498f72
ZechCodes <hi@zech.codes>
2023-04-13 19:24:18 -0400

    Update tests to properly setup the repository

--- tests/test_bevy.py
+++ tests/test_bevy.py
@@ -1,64 +1,68 @@
 from bevy import dependency, inject, Repository, get_repository
 from bevy.type_provider import TypeProvider
 
+from pytest import fixture
 
-def test_repository_exists():
+
+@fixture
+def repository():
+    Repository._bevy_repository.set(Repository())
+    return get_repository()
+
+
+def test_repository_exists(repository):
     assert isinstance(get_repository(), Repository)
 
 
-def test_repository_get():
+def test_repository_get(repository):
     class TestType:
         ...
 
-    repo = get_repository()
-    repo.add_providers(TypeProvider)
-    instance = repo.get(TestType)
+    repository.add_providers(TypeProvider)
+    instance = repository.get(TestType)
 
     assert isinstance(instance, TestType)
 
 
-def test_repository_caching():
+def test_repository_caching(repository):
     class TestType:
         ...
 
-    repo = get_repository()
-    repo.add_providers(TypeProvider)
-    instance_a = repo.get(TestType)
-    instance_b = repo.get(TestType)
+    repository.add_providers(TypeProvider)
+    instance_a = repository.get(TestType)
+    instance_b = repository.get(TestType)
 
     assert instance_a is instance_b
 
 
-def test_injection_descriptor():
+def test_injection_descriptor(repository):
     class Dep:
         ...
 
     class TestType:
         dep: Dep = dependency()
 
-    repo = get_repository()
-    repo.add_providers(TypeProvider)
+    repository.add_providers(TypeProvider)
     instance = TestType()
 
     assert isinstance(instance.dep, Dep)
 
 
-def test_injection_descriptor_is_shared():
+def test_injection_descriptor_is_shared(repository):
     class Dep:
         ...
 
     class TestType:
         dep: Dep = dependency()
 
-    repo = get_repository()
-    repo.add_providers(TypeProvider)
+    repository.add_providers(TypeProvider)
     instance_a = TestType()
     instance_b = TestType()
 
     assert instance_a.dep is instance_b.dep
 
 
-def test_function_parameter_injection():
+def test_function_parameter_injection(repository):
     class DepA:
         ...
 
@@ -68,6 +72,8 @@ def test_function_parameter_injection():
     @inject
     def test_function(param_a: DepA = dependency(), param_b: DepB = dependency()):
         return param_a, param_b
+
+    repository.add_providers(TypeProvider)
 
     ret_a, ret_b = test_function()
     assert isinstance(ret_a, DepA)
