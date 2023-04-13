dc435892863fb314cdf934753e9c7a35e1321970
ZechCodes <hi@zech.codes>
2023-04-13 18:59:12 -0400

    Update type provider types

--- bevy/type_provider.py
+++ bevy/type_provider.py
@@ -1,11 +1,11 @@
-from bevy.providers import Provider, Builder
-from typing import Type, TypeVar
+from bevy.providers import Provider
+from typing import Callable, Type, TypeVar
 
 
-T = TypeVar("_T")
+_T = TypeVar("_T")
 
 
-class TypeProvider(Provider):
+class TypeProvider(Provider[Type[_T], _T]):
     """The type provider supports any types and will attempt to instantiate them with no args."""
-    def builder(self, new_type: Type[T]) -> Builder | None:
+    def builder(self, new_type: Type[_T]) -> Callable[[], _T] | None:
         return lambda: new_type()
