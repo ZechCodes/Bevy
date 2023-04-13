c01f10327d0e9df2319a423a5523f1f7bcd1bca5
ZechCodes <hi@zech.codes>
2023-04-13 19:46:06 -0400

    Rename provider lookup param to key

--- bevy/providers.py
+++ bevy/providers.py
@@ -3,34 +3,34 @@ from typing import Callable, Generic, TypeVar
 from abc import ABC, abstractmethod
 
 
-_T = TypeVar("_T")
+_K = TypeVar("_K")
 _V = TypeVar("_V")
 Builder = Callable[[], _V]
 
 
-class Provider(Generic[_T, _V], ABC):
+class Provider(Generic[_K, _V], ABC):
     def __init__(self, repository):
         self._repository = repository
-        self._cache: dict[_T, _V] = {}
+        self._cache: dict[_K, _V] = {}
 
     @abstractmethod
-    def builder(self, new_type: _T) -> Builder | None:
+    def builder(self, key: _K) -> Builder | None:
         """Should create a function to build an instance of the type, or returns None if the type is not supported."""
 
-    def create(self, new_type: _T) -> Result[_V]:
+    def create(self, key: _K) -> Result[_V]:
         with ResultBuilder() as (result_builder, set_result):
-            if (builder := self.builder(new_type)) is None:
-                raise Exception(f"The provider does not support {new_type!r}")
+            if (builder := self.builder(key)) is None:
+                raise Exception(f"The provider does not support {key!r}")
 
-            self._cache[new_type] = set_result(builder())
+            self._cache[key] = set_result(builder())
 
         return result_builder.result
 
-    def find(self, lookup_type: _T) -> Result[_V]:
+    def find(self, key: _K) -> Result[_V]:
         with ResultBuilder() as (builder, set_result):
             try:
-                set_result(self._cache[lookup_type])
+                set_result(self._cache[key])
             except KeyError as exception:
-                raise Exception(f"Provider had no instances cached for {lookup_type!r}") from exception
+                raise Exception(f"Provider had no instances cached for {key!r}") from exception
 
         return builder.result
