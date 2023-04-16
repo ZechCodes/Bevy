0c4aba2b3fca8a286d3c6363c9bbecca0d23fa05
ZechCodes <hi@zech.codes>
2023-04-16 16:24:58 -0400

    Update the default factory contextvar to use Options

--- bevy/contextvar.py
+++ bevy/contextvar.py
@@ -1,9 +1,9 @@
 from contextvars import ContextVar as _ContextVar, Token as _Token
 from typing import Callable, Generic, TypeVar
 
+from bevy.options import Value as _Value, Null as _Null
 
 _T = TypeVar("_T")
-_NOTSET = object()
 
 
 class ContextVarDefaultFactory(Generic[_T]):
@@ -11,28 +11,29 @@ class ContextVarDefaultFactory(Generic[_T]):
     set."""
 
     def __init__(self, name: str, *, default: Callable[[], _T]):
-        self._var = _ContextVar(name, default=_NOTSET)
+        self._var = _ContextVar(name, default=_Null())
         self._factory = default
 
     @property
     def name(self) -> str:
-        """Proxy the name of the contextvar"""
+        """Proxy the name of the contextvar."""
         return self._var.name
 
     def get(self) -> _T | None:
         """Proxy getting the value of the contextvar, setting the contextvar's value using the factory function if the
         contextvar is not already set."""
-        value = self._var.get()
-        if value is not _NOTSET:
-            return value
+        match self._var.get():
+            case _Value(value):
+                return value
 
-        new_value = self._factory()
-        self._var.set(new_value)
-        return new_value
+            case _Null():
+                value = self._factory()
+                self.set(value)
+                return value
 
     def set(self, value: _T):
         """Proxy the contextvar's set method."""
-        self._var.set(value)
+        self._var.set(_Value(value))
 
     def reset(self, token: _Token[_T]) -> None:
         """Proxy the contextvar's reset method."""
