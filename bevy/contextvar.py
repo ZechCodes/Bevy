03793d3b82313c6a3ab804601c821e7c1e7fcd18
ZechCodes <hi@zech.codes>
2023-04-16 16:21:22 -0400

    Document the default factory contextvar

--- bevy/contextvar.py
+++ bevy/contextvar.py
@@ -7,15 +7,21 @@ _NOTSET = object()
 
 
 class ContextVarDefaultFactory(Generic[_T]):
+    """This contextvar wrapper uses a factory function to create the value of the context variable if it isn't already\
+    set."""
+
     def __init__(self, name: str, *, default: Callable[[], _T]):
         self._var = _ContextVar(name, default=_NOTSET)
         self._factory = default
 
     @property
     def name(self) -> str:
+        """Proxy the name of the contextvar"""
         return self._var.name
 
     def get(self) -> _T | None:
+        """Proxy getting the value of the contextvar, setting the contextvar's value using the factory function if the
+        contextvar is not already set."""
         value = self._var.get()
         if value is not _NOTSET:
             return value
@@ -25,7 +31,9 @@ class ContextVarDefaultFactory(Generic[_T]):
         return new_value
 
     def set(self, value: _T):
+        """Proxy the contextvar's set method."""
         self._var.set(value)
 
     def reset(self, token: _Token[_T]) -> None:
+        """Proxy the contextvar's reset method."""
         self._var.reset(token)
