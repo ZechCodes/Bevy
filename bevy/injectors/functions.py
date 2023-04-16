b4143252096cabc918b80dd32f4deceb0453cc9a
ZechCodes <hi@zech.codes>
2023-04-16 15:53:33 -0400

    Document the injection decorator

--- bevy/injectors/functions.py
+++ bevy/injectors/functions.py
@@ -11,9 +11,17 @@ _C: TypeAlias = Callable[_P, _R]
 
 
 def inject(func: _C) -> Callable[_P, _R]:
+    """This decorator creates a wrapper function that scans the decorated function for dependencies. When the function
+    is later called any dependency parameters that weren't passed in will be injected by the wrapper function. This can
+    intelligently handle positional only arguments, positional arguments, named arguments, and keyword only arguments.
+
+    All parameters that should be injected need to be given a type hint that is supported by a provider and must have a
+    default value that is an instance of `bevy.injectors.classes.Dependency`.
+    """
     sig = signature(func)
     ns = _get_function_namespace(func)
     annotations = get_annotations(func, globals=ns, eval_str=True)
+    # Determine which parameters have a declared dependency
     inject_parameters = {
         name: annotations[name]
         for name, parameter in sig.parameters.items()
@@ -24,6 +32,7 @@ def inject(func: _C) -> Callable[_P, _R]:
     def injector(*args: _P.args, **kwargs: _P.kwargs) -> _R:
         params = sig.bind_partial(*args, **kwargs)
         repo = get_repository()
+        # Get instances from the repo to fill all missing dependency parameters
         params.arguments |= {
             name: repo.get(dependency_type)
             for name, dependency_type in inject_parameters.items()
@@ -35,10 +44,12 @@ def inject(func: _C) -> Callable[_P, _R]:
 
 
 def _get_function_namespace(func: _C) -> dict[str, Any]:
+    """Get the variables that the function had in its name space."""
     return _unwrap_function(func).__globals__
 
 
 def _unwrap_function(func: _C) -> _C:
+    """Attempt to unwrap a function from all decorators."""
     if hasattr(func, "__func__"):
         return _unwrap_function(func.__func__)
 
