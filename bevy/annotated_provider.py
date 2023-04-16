35a4121cc48d93eb55980ceab97230823f18e240
ZechCodes <hi@zech.codes>
2023-04-16 12:42:31 -0400

    Document the annotated provider

--- bevy/annotated_provider.py
+++ bevy/annotated_provider.py
@@ -18,6 +18,7 @@ _A: TypeAlias = Annotated[Type[_T], Hashable]
 
 
 def get_type(annotated: _A) -> Option[_T]:
+    """Get the type being annotated by a typing.Annotated instance."""
     match get_args(annotated):
         case (type() as type_, _):
             return Value(type_)
@@ -26,9 +27,13 @@ def get_type(annotated: _A) -> Option[_T]:
 
 
 class AnnotatedProvider(Provider[_A, _T]):
-    """The type provider supports any types and will attempt to instantiate them with no args."""
+    """The annotated provider supports typing.Annotated annotations. It will attempt to instantiate the annotated type
+    if it's not found in the cache."""
 
     def factory(self, annotated: _A) -> Option[Callable[[], _T]]:
+        """Get a callable for getting or constructing an instance of the annotated type. This will call the repository's
+        get method looking up the un-annotated type, this will attempt to instantiate an instance of the type if no
+        providers have an instance cached."""
         match get_type(annotated):
             case Value(type_):
                 return Value(partial(self._repository.get, type_))
