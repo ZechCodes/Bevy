04c904116e6c8b6ec2cbaeaf5de9e6c3b0f59d86
ZechCodes <hi@zech.codes>
2023-04-16 11:20:28 -0400

    Add support for custom bevy constructor methods

--- bevy/type_provider.py
+++ bevy/type_provider.py
@@ -1,12 +1,23 @@
-from bevy.providers import Provider
-from typing import Callable, Type, TypeVar
+from typing import Callable, Type, TypeVar, Protocol, runtime_checkable
 
+from bevy.providers import Provider
 
 _T = TypeVar("_T")
+
+
+@runtime_checkable
+class BevyConstructable(Protocol[_T]):
+    @classmethod
+    def __bevy_constructor__(cls: Type[_T], *args, **kwargs) -> _T:
+        ...
 
 
 class TypeProvider(Provider[Type[_T], _T]):
     """The type provider supports any types and will attempt to instantiate them with no args."""
 
     def factory(self, new_type: Type[_T]) -> Callable[[], _T] | None:
-        return lambda: new_type()
+        match new_type:
+            case BevyConstructable():
+                return new_type.__bevy_constructor__
+            case _:
+                return new_type
