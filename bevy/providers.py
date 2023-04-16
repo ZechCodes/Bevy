7bf2cf5b021c37e3c13dd6161e422b8a7751dbc0
ZechCodes <hi@zech.codes>
2023-04-16 14:25:22 -0400

    Rewrite providers & repository to use options

--- bevy/providers.py
+++ bevy/providers.py
@@ -1,64 +1,51 @@
-from abc import ABC, abstractmethod
 from typing import Callable, Generic, TypeVar
 
-from bevy.options import Option, Value
-from bevy.results import Result, result
+from bevy.options import Option, Null, Value
 
 _K = TypeVar("_K")
 _V = TypeVar("_V")
 Factory = Callable[[], _V]
 
 
-class _MessageResult(Result):
-    __match_args__ = ("message",)
-
-    def __init__(self, message: str):
-        self.message = message
-
-    def __bool__(self):
-        return False
+class NotFound(Null):
+    """Null option for when a key isn't found in a provider's cache."""
 
 
-class NotFound(_MessageResult):
-    ...
+class NotSupported(Null):
+    """Null option for when a key isn't supported by a provider."""
 
 
-class NotSupported(_MessageResult):
-    ...
+class Provider(Generic[_K, _V]):
 
 
-class Provider(Generic[_K, _V], ABC):
     def __init__(self, repository):
         self._repository = repository
         self._cache: dict[_K, _V] = {}
 
-    @abstractmethod
     def factory(self, key: _K) -> Option[Factory]:
         """Should create a function to build an instance of the type, or returns None if the type is not supported."""
+        return Null(f"{type(self)!r} does not support creating instances of {key!r}")
 
-    @result
-    def create(self, key: _K) -> _V | NotSupported:
+    def create(self, key: _K) -> Option[_V]:
         match self.factory(key):
-            case Value(factory):
+            case Value(factory) if self.supports(key):
                 self._cache[key] = factory()
-                return self._cache[key]
+                return Value(self._cache[key])
             case _:
                 return NotSupported(f"{type(self)!r} does not support {key!r}")
 
-    @result
-    def find(self, key: _K) -> _V | NotFound:
+    def find(self, key: _K) -> Option[_V]:
         try:
-            return self._cache[key]
+            return Value(self._cache[key])
         except KeyError as exception:
             return NotFound(f"{type(self)!r} has no instances cached for {key!r}")
 
-    @result
-    def set(self, key: _K, value: _V) -> _V | Result:
+    def set(self, key: _K, value: _V) -> Option[_V]:
         if not self.supports(key):
             return NotSupported(f"{type(self)!r} does not support {key!r}")
 
         self._cache[key] = value
-        return value
+        return Value(value)
 
     def supports(self, key: _K) -> bool:
         return bool(self.factory(key))
--- bevy/repository.py
+++ bevy/repository.py
@@ -1,8 +1,8 @@
-from bevy.providers import Provider
-from bevy.results import Result, Success, Failure
 from typing import Generic, TypeVar, Type
-from bevy.contextvar import ContextVarDefaultFactory as _ContextVarDefaultFactory
 
+from bevy.contextvar import ContextVarDefaultFactory as _ContextVarDefaultFactory
+from bevy.options import Option, Value, Null
+from bevy.providers import Provider
 
 _K = TypeVar("_K")
 _V = TypeVar("_V")
@@ -24,45 +24,57 @@ class Repository(Generic[_K, _V]):
         instances that will be stored and returned by the repository."""
         self._providers.extend(provider(self) for provider in providers)
 
-    def create(self, key: _K) -> _V | None:
-        """Attempts to create an instance of the type, returning None if no supporting provider was found."""
+    def create(self, key: _K) -> Option[_V]:
+        """Attempts to create an instance that adheres to the type _V and that corresponds to the key by looking for a
+        provider that supports the key. Returns a Null option when no provider is found for the key, otherwise returns a
+        Value option containing the instance created by the provider."""
         for provider in self._providers:
             match provider.create(key):
-                case Success(result):
-                    return result
+                case Value(result):
+                    return Value(result)
 
-        return None
+        return Null(f"No providers supported the {key!r}")
 
-    def find(self, key: _K) -> Result[_V]:
-        """Gets an instance from the repository, returning None if no match was found for the lookup type."""
+    def find(self, key: _K) -> Option[_V]:
+        """Searches all providers for a cached instance that adheres to the type _V and that corresponds to the key.
+        A Value option containing the matching instance is returned, when no match is found a Null option is returned.
+        """
         for provider in self._providers:
             match provider.find(key):
-                case Success(_) as result:
+                case Value() as result:
                     return result
 
-        return Failure(Exception(f"No match for {key!r} was found in the repository"))
+        return Null(f"No match for {key!r} was found in the repository")
 
-    def get(self, key: _K) -> _V | None:
-        """Gets an object from the repository, creating it if it doesn't exist and a provider is found that supports it,
-        otherwise it will return None."""
+    def get(self, key: _K, default: _V | None = None) -> _V:
+        """Attempts to get an instance adhering to the type _V that corresponds to the key. It first attempts to find a
+        matching instance that is cached on any provider. If no cached instances are found, it attempts to create an
+        instance that is then cached. When a match is found or created it is returned, otherwise the default is used.
+        """
         match self.find(key):
-            case Success(result):
+            case Value(result):
                 return result
-            case Failure(_):
-                return self.create(key)
+            case Null():
+                return self.create(key).value_or(default)
 
-    def set(self, key: _K, value: _V):
+    def set(self, key: _K, value: _V) -> Option[_V]:
+        """Attempts to cache a value. A Null option is returned when no providers support the key, otherwise a Value
+        option containing the value that was placed in the cache is returned."""
         for provider in self._providers:
             match provider.set(key, value):
-                case Success(_):
-                    return
+                case Value(cached_value):
+                    return Value(cached_value)
+
+        return Null()
 
     @classmethod
     def get_repository(cls: "Type[Repository[_K, _V]]") -> "Repository[_K, _V]":
+        """Retrieves the repository instance that is assigned to the current context."""
         return cls._bevy_repository.get()
 
     @classmethod
     def set_repository(cls, repository: "Repository[_K, _V]"):
+        """Assigns a new repository instance to the current context."""
         cls._bevy_repository.set(repository)
 
 
