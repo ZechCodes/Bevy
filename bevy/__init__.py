6903c2c3d96bdbc86214c00a8637fc22efb16ad6
ZechCodes <hi@zech.codes>
2023-04-23 16:30:14 -0400

    Correct incorrect import

--- bevy/__init__.py
+++ bevy/__init__.py
@@ -1,5 +1,5 @@
 from bevy.injectors.classes import dependency
+from bevy.injectors.functions import inject
 from bevy.repository import Repository, get_repository
-from injectors.functions import inject
 
 __all__ = ("dependency", "inject", "Repository", "get_repository")
