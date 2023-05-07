0f32c9d5cceb5c3f46ceff047aac4a2afdaf5e5a
ZechCodes <hi@zech.codes>
2023-05-07 18:58:55 -0400

    Improve options docs

--- bevy/options.py
+++ bevy/options.py
@@ -5,24 +5,34 @@ _T = TypeVar("_T")
 
 
 class Option(Generic[_T], ABC):
-    """Option types help make code cleaner and typing more consistent. The value type can wrap values while the null
-    type can stand in for anytime that there is no value set. Option types are convenient to use with match/case
-    statements."""
+    """`Option` types help make code cleaner and typing more consistent. The `Value` option wraps values while the `Null`
+    options stand in for no value. `Option` types are convenient to use with match/case statements.
+
+    **Example**
+
+        match result:
+            case Value(value):
+                ...
+            case Null():
+                ...
+    """
 
     @property
     @abstractmethod
     def value(self) -> _T:
-        return self._get_value()
+        """Returns the value assigned to the option. This raises `Exception` if accessed on a `Null` option."""
 
     @abstractmethod
     def value_or(self, default: _T) -> _T:
-        """Returns the value if it is set or returns the provided default value."""
+        """Returns the value if it is set, otherwise returns the provided default value."""
 
 
 class Value(Option[_T]):
-    """The Value option type is used to wrap a set value. No matter what the contained value is instances of the Value
-    type will always evaluate as truthyto help identify if an Option has a value or not. The Value type can surface its
-    value as a match arg in a match/case statement.
+    """The `Value` option type is used to wrap a value. No matter what the value a `Value` option always evaluates as
+    `True` to help identify if an `Option` has a value set. The `Value` option can surface its value as a match arg in a
+    match/case statement.
+
+    **Example**
 
         match example:
             case Value(value):
@@ -36,10 +46,11 @@ class Value(Option[_T]):
 
     @property
     def value(self) -> _T:
+        """Gets the value assigned to the `Value` option."""
         return self._value
 
     def value_or(self, default: _T) -> _T:
-        """The Value option will always have a value so this just returns the value."""
+        """`Value` option always have a value set, this always returns that value."""
         return self.value
 
     def __bool__(self):
@@ -53,9 +64,11 @@ class Value(Option[_T]):
 
 
 class Null(Option):
-    """The Null option type represents the absence of a value. The Null option will always evaluate as falsey to help
-    identify if an Option has a value or not. Null options can take an optional message that will be used in error
-    messages and that can be surfaced as a match arg in match/case statements.
+    """The `Null` option type represents the absence of a value. The `Null` option will always evaluate as `False` to
+    help identify if an `Option` has a value or not. `Null` options can take an optional message that will be used in
+    error messages and that can be surfaced as a match arg in match/case statements.
+
+    **Example**
 
         match example:
             case Null(message):
@@ -69,14 +82,16 @@ class Null(Option):
 
     @property
     def message(self) -> str:
-        return self._message or "Null value"
+        """The exception message the `Null` option will use if the value is accessed."""
+        return self._message or "The value is null"
 
     @property
     def value(self):
+        """The `Null` options never have values so this raises an `Exception` containing the `Null` option's message."""
         raise Exception(self.message)
 
     def value_or(self, default: _T) -> _T:
-        """The Null option will never have a value so this just returns the default."""
+        """`Null` options never have a value so this only returns `default`."""
         return default
 
     def __bool__(self):
