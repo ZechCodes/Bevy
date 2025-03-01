e24920f3fc556555b1343b983f59dd4b71a7f0b6
Zech Zimmerman <hi@zech.codes>
2025-02-28 20:30:53 -0500

    Add docstrings to factories module

--- bevy/factories.py
+++ bevy/factories.py
@@ -1,11 +1,12 @@
 from collections.abc import Callable
 from functools import update_wrapper
-from typing import Sequence, Type
 from typing import overload, Sequence, Type
 import bevy.registries as r
 
 
 class Factory[**P, T]:
+    """A wrapper for dependency factories. This makes it easier to define factories that can handle various types and
+    add them to the registry."""
     def __init__(self, dependency_types: Sequence[Type[T]], factory: "r.DependencyFactory[P, T]"):
         self.dependency_types = dependency_types
         self.factory = factory
@@ -22,12 +23,15 @@ class Factory[**P, T]:
         ...
 
     def register_factory(self, registry: "r.Registry | None" = None):
+        """Adds the factory to the registry for each dependency type the factory supports. If the registry is not
+        provided or is None the global registry will be used."""
         registry = r.get_registry(registry)
         for dependency_type in self.dependency_types:
             registry.add_factory(self, dependency_type)
 
 
 def factory[**P, T](*dependency_types: Type[T]) -> "Callable[[r.DependencyFactory[P, T]], r.DependencyFactory[P, T]]":
+    """Decorator that wraps a factory function in a factory wrapper."""
     def decorator(dependency_factory: Factory[P, T]) -> r.DependencyFactory[P, T]:
         wrapper = Factory[P, T](dependency_types, dependency_factory)
         wrapper = update_wrapper(wrapper, dependency_factory)
