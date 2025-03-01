22857df104441df467d872677e92a7229568172f
Zech Zimmerman <hi@zech.codes>
2025-02-28 20:06:54 -0500

    Add a docstring for the type factory hook

--- bevy/bundled/type_factory_hook.py
+++ bevy/bundled/type_factory_hook.py
@@ -8,6 +8,7 @@ from bevy.hooks import hooks
 
 @hooks.HANDLE_UNSUPPORTED_DEPENDENCY
 def type_factory[T](container: Container, dependency: Type[T]) -> Optional[T]:
+    """This hook adds a dependency factory that injects an instances of the dependency type."""
     if isinstance(dependency, type):
         return Optional.Some(container.call(dependency))
 
