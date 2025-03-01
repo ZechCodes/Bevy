fd29f584aafc455084a666359a0a18d4091b6c03
Zech Zimmerman <hi@zech.codes>
2025-02-28 19:56:01 -0500

    Add doc strings to dependencies module

--- bevy/dependencies.py
+++ bevy/dependencies.py
@@ -6,6 +6,8 @@ if TYPE_CHECKING:
 
 
 class DependencyMetadata:
+    """Dependency metadata wrapper. A factory function can be provided for creating the dependency, this factory
+    function must take a container instance as a parameter."""
     def __init__(self, factory: "Callable[[Container], Any] | None" = None):
         self.factory = factory
 
@@ -21,4 +23,6 @@ def dependency[T](factory: "Callable[[Container], T] | Factory[None, T]") -> T:
 
 
 def dependency[T](factory: "Callable[[Container], T] | Factory[None, T] | None" = None) -> T | Any:
+    """Intended to be called as a parameter default value. It assigns the default to a dependency metadata object. A
+    factory function can be passed that returns an instance of the dependency type."""
     return DependencyMetadata(factory)
