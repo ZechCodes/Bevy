c8d52b3c903aa73a2a3d4d602221050f4c9495c3
ZechCodes <hi@zech.codes>
2025-03-09 18:51:22 -0400

    Add get_container to top level import

--- bevy/__init__.py
+++ bevy/__init__.py
@@ -1,5 +1,6 @@
+from bevy.containers import get_container
 from bevy.dependencies import dependency
 from bevy.injections import inject
-from bevy.registries import get_registry, Registry
+from bevy.registries import get_registry
 
-__all__ = ["Registry", "get_registry", "dependency", "inject"]
+__all__ = ["get_registry", "get_container", "dependency", "inject"]
