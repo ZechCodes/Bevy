ef1498d388157600c0a6261329759932b77323de
Zech Zimmerman <hi@zech.codes>
2025-05-30 12:46:15 -0400

    Include the container and registry types in the package level imports

--- bevy/__init__.py
+++ bevy/__init__.py
@@ -1,6 +1,6 @@
-from bevy.containers import get_container
+from bevy.containers import get_container, Container
 from bevy.dependencies import dependency
 from bevy.injections import inject
-from bevy.registries import get_registry
+from bevy.registries import get_registry, Registry
 
 __all__ = ["get_registry", "get_container", "dependency", "inject"]
